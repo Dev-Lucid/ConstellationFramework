d1c178b78d7137e3a3579dbb022bbdd0717c5fd0
Dev-Lucid <mthorn@devlucid.com>
2013-07-22 11:07:15 -0400

    changing less build to lessc instead of recess

--- bin/build/includes-css.php
+++ bin/build/includes-css.php
@@ -5,6 +5,8 @@ echo("Building CSS...");
 # first, build the entry point for all of the less.
 $entry_less = __DIR__.'/../../../../www/media/less/all-'.md5(time()).'.less';
 $to_compile .= '@import "bootstrap";'."\n";
+$to_compile .= '@import "bsc-data-table";'."\n";
+$to_compile .= '@import "bsc-forms";'."\n";
 if(file_exists(__DIR__.'/../../../../www/media/less/customizations.less'))
 {
 	$to_compile .= '@import "customizations";'."\n";
@@ -17,22 +19,22 @@ try
 {
 
 
-	$cmd = "recess --compile  --includePath=".__DIR__."/../../../../lib/TwitterBootstrap/less/:".__DIR__."/../../../../www/media/less/ ".$entry_less;
+	$include_path  = __DIR__."/../../../../lib/TwitterBootstrap/less/:";
+	$include_path .= __DIR__."/../../../../www/media/less/:";
+	$include_path .= __DIR__."/../../../../lib/BootstrapConstructor/lib/less/";
+	
+	$cmd = "lessc --compile  --include-path=".$include_path."  ".$entry_less;
 	if(file_exists(__DIR__.'/../../www/media/combined.css'))
 		unlink(__DIR__.'/../../www/media/combined.css');
+	#echo($cmd);
 	$final_uncompressed_css = shell_exec($cmd);
 
-	$cmd = "recess --compile --compress --includePath=".__DIR__."/../../../../lib/TwitterBootstrap/less/ ".$entry_less;
+	$cmd = "lessc --compile --compress --include-path=".$include_path." ".$entry_less;
 	if(file_exists(__DIR__.'/../../../../www/media/combined.min.css'))
 		unlink(__DIR__.'/../../../../www/media/combined.min.css');
 	$final_compressed_css = shell_exec($cmd);	
 	
 
-	# include some misc stylesheets
-	$final_uncompressed_css .= file_get_contents(__DIR__.'/../../../../lib/BootstrapConstructor/lib/css/data-table.css');
-	$final_compressed_css .= file_get_contents(__DIR__.'/../../../../lib/BootstrapConstructor/lib/css/data-table.css');
-	$final_uncompressed_css .= file_get_contents(__DIR__.'/../../../../lib/BootstrapConstructor/lib/css/forms.css');
-	$final_compressed_css .= file_get_contents(__DIR__.'/../../../../lib/BootstrapConstructor/lib/css/forms.css');
 
 	
 	# build/fix the font awesome css.
